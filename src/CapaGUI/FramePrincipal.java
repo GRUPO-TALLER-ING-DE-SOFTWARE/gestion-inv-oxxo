eb53d14ec34793fc656c1e31b6d3c53fc986c9dd
PesoPalta <sfloresg1@correo.uss.cl>
2024-10-21 13:59:32 -0300

    Cambio de main

--- src/CapaGUI/FramePrincipal.java
+++ src/CapaGUI/FramePrincipal.java
@@ -141,7 +141,7 @@ public class FramePrincipal extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new FrameInventario().setVisible(true);
+                new FramePrincipal().setVisible(true);
             }
         });
     }
