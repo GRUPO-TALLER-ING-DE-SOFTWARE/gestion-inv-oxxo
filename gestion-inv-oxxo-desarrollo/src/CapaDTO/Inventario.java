6a62122b88d419d1624ad98a9543c2dc88b36437
NthEli <bravoelias626@gmail.com>
2024-11-24 17:48:20 -0300

    Add files via upload

---
+++ gestion-inv-oxxo-desarrollo/src/CapaDTO/Inventario.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CapaDTO;
+
+/**
+ *
+ * @author cbas2
+ */
+public class Inventario {
+    private int id; // Primary key del inventario, auto incremental no nulo
+    private String nombre; // Varchar(50) not null, esto es de producto, hay que editar
+    private String categoria; // Varchar(25) not null, lo mismo que el de arriba
+    private int cantidad; // int not null
+    private String estado; // Varchar(25) not null
+    private float precio; // float not null, tambien viene desde producto
+    private int nivel_reorden; // int not null
+    /*Recomiendo para este objeto hacer lo siguiente:
+    - Borrar todos los getter and setter para luego hacerlos cuando hagamos bien los datos.
+    - Borrar todos los datos que vienen desde la tabla producto, y traer solo la primary key como foreign key.
+
+    TABLAS SQL:
+create table productos (id int primary key auto_increment not null, nombre varchar(50) not null, categoria varchar(50), precio float not null);
+create table inventario (id int primary key auto_increment not null, id_producto int not null, estado int not null, nivel_reorden int not null, foreign key (id_producto) references productos(id));
+create table stock (id int primary key auto_increment not null, id_producto int not null, cantidad int not null, foreign key (id_producto) references productos(id));
+create table venta (id int primary key auto_increment not null, id_producto int not null, cantidad int not null, precio_total float not null, fecha date not null, foreign key (id_producto) references productos(id));
+
+    */
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public int getNivel_reorden() {
+        return nivel_reorden;
+    }
+
+    public void setNivel_reorden(int nivel_reorden) {
+        this.nivel_reorden = nivel_reorden;
+    }
+}
---
+++ gestion-inv-oxxo-desarrollo/src/CapaNegocio/NegocioAlertas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CapaNegocio;
+
+/**
+ *
+ * @author Lab301
+ */
+public class NegocioAlertas {
+    
+    private void CrearAlerta(){
+        
+    }
+}
